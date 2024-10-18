e8b121c9bf6761e8e7c8fcc1e1d341cabff502b0
2Cnieh52 <00001094836977sp@al.educacao.sp.gov.br>
2024-10-18 18:45:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,38 @@
+/*NOEMI GOMES DE OLIVEIRA _ 2c. 16/10/24*/
+
+
+criaCartao(
+    'Teoria Musical',
+    'O que é o som?',
+    'É toda a vibração detectada pelo ouvido humano.'
+)
+
+criaCartao(
+   'Teoria Musical',
+   'O que é o som musical e o não musical?',
+   'Som musical: São vibrações sonoras regulares e uniformes que podem ser grafados. Som não musical: É o reuído, resulta de vibrações inregulares, não podem ser grafados.'
+)
+
+criaCartao(
+  'Teoria Musical',
+  'Quantas e quais são as propriedades do som?',
+  'São 4. 1: Altura. 2: Duração. 3: Intensidade. 4: Timbre.'
+)
+
+criaCartao(
+    'Teoria Musical',
+    'Defina as propriedades do som!',
+    'Altura: Possibilita o som ser + grave/agudo. Duração: Tempo que se prolonga o som. Intensidade: Volume do som + forte/fraco. Timbre: Distingue sua origem.'
+)
+
+criaCartao(
+    'Teoria Musical',
+    'O que é a música?',
+    'É a arte de manifestar os diversos afetos da nossa alma mediante ao som.'
+)
+
+criaCartao(
+    'Teoria Musical',
+    'Quais são os elementos da música?',
+    'Melodia: combinação de sons sucessivos. Harmonia: Combinação de sons simultâñeos. Ritmo: Combinação dos valores no discurso musical, regulados. Notação musical: Escrita. '
+)
